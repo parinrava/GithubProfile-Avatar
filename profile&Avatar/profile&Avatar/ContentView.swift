72b24bff3e33f2a401d1c4bc20f501022b47e1fb
parinrava <122297950+parinrava@users.noreply.github.com>
2024-10-11 14:25:57 -0700

    initial commit

---
+++ profile&Avatar/profile&Avatar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  profile&Avatar
+//
+//  Created by Parin Ravanbakhsh on 2024-10-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+          
+        }
+        .padding()
+    }
+}
+
+//#Preview {
+//    ContentView()
+//}
---
+++ profile&Avatar/profile&Avatar/profile_AvatarApp.swift
@@ -0,0 +1,17 @@
+//
+//  profile_AvatarApp.swift
+//  profile&Avatar
+//
+//  Created by Parin Ravanbakhsh on 2024-10-11.
+//
+
+import SwiftUI
+
+@main
+struct profile_AvatarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
