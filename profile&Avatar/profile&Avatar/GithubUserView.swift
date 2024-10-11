bb28812b82a96d4a94aaff444d7625c23f1ab7bd
parinrava <122297950+parinrava@users.noreply.github.com>
2024-10-11 14:51:30 -0700

    added the logic to start the task using the url session to fetch the data from github api

--- profile&Avatar/profile&Avatar/GithubUserView.swift
+++ profile&Avatar/profile&Avatar/GithubUserView.swift
@@ -19,7 +19,25 @@ class GithubUserView: ObservableObject {
         guard let url = URL (string:"https://api.github.com/users/parinrava") else {
             return
         }
-    }
 
+    
+    //start task using the url session to fetch
+    URLSession.shared.dataTask(with: url) { data, response, error in
+        if let data = data { // if the data successfully fetched
+            do {
+                //use the json decoder to decode the json response
+                let decoder = JSONDecoder()
+                let user = try decoder.decode(GithubUser.self, from:data)
+                //update the user property on the main thread to refresh the UI
+                DispatchQueue.main.async{
+                    self.user = user
+                }
+            }catch{
+                //parint error message
+                print("error decoding json:", error)
+            }
+        }
+        }.resume() //resume the data task to start
+    }
 }
 
